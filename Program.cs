0285cfc5c0659835a7d220f6896cfc4e22d76944
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-01-20 15:49:37 +0530

    [Chandra]- LinkedList

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructuresdemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Linkedlist list = new Linkedlist();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.InsertAtParticularPosition(2, 51);
+            list.RemoveFirstNode();
+            list.Search(51);
+            list.RemoveLastNode();
+            list.Display();
+        }
+    }
+}
